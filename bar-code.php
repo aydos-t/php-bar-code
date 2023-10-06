547ed3fb038b6e506cefeeda3b43cf2d83c44639
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-10-06 13:50:47 +0500

    sql query to database

--- bar-code.php
+++ bar-code.php
@@ -4,7 +4,21 @@ require_once 'vendor/autoload.php';
 $barcode = time().".png";
 $barimage = time().".png";
 $color = [255,0,0];
+if (isset($_REQUEST['generate-btn'])) {
+    $bartext = $_REQUEST['bartext'];
+    $query = mysqli_query($connect, "insert into php-bar-code set bartext='$bartext', barimage='$barimage'");
+    if ($query) {
+        {
+            ?>
+            <script>
+                alert("Data save successfully");
+            </script>
+            <?php
+        }
+    }
+}
 
 $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
-file_put_contents('barcode.png', $generator->getBarcode('081231723897', $generator::TYPE_CODE_128, 3, 50, $redColor));
+file_put_contents($barcode, $generator -> getBarcode($bartext, $generator ::TYPE_CODE_128, 3, 50, $color));
+echo "<center><img src='".$barcode."'></center>";
 ?>
